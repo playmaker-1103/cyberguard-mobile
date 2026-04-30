5634426be861db09da7af980499d202e5c85623e
Nguyễn Tiến Đạt <tiendat11032005@gmail.com>
2026-04-30 13:22:26 +0100

    Initial Commit

---
+++ CyberGuard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CyberGuard
+//
+//  Created by Đạt Nguyễn Tiến on 30/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CyberGuard/CyberGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyberGuardApp.swift
+//  CyberGuard
+//
+//  Created by Đạt Nguyễn Tiến on 30/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CyberGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
